3e199268b77e4f1f957256fddabdee7d64c2e2ef
Praveen Shrivas <47313992+praveenshrivas@users.noreply.github.com>
2025-10-26 17:33:16 +0530

    Update main.tf
    test

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-0912f71e06545ad88"
+  ami           = "ami-0912f71e06545ad89"
   instance_type = "t2.micro"
   tags = {
     Name = "Webhook-Demo"
